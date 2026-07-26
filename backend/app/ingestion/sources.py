a5f088a19e67fa64b8e6e5449d1577dbaa361278
Amy (ZiQing) Zeng <azamyzeng@gmail.com>
2026-07-26 14:44:00 -0700

    Update news sources list

--- backend/app/ingestion/sources.py
+++ backend/app/ingestion/sources.py
@@ -1,8 +1,9 @@
 # 新闻源配置。以后增删源改这里就行。
 
 NEWS_SOURCES = [
-    "https://feeds.bbci.co.uk/news/world/rss.xml",
-    "https://www.aljazeera.com/xml/rss/all.xml",
-    "https://www.cnbc.com/id/100003114/device/rss/rss.html",
-    "https://feeds.bbci.co.uk/news/technology/rss.xml",
+    "https://feeds.bbci.co.uk/news/world/rss.xml", #BBC 世界
+    "https://www.aljazeera.com/xml/rss/all.xml", #半岛电视台,国际时政强
+    "https://www.cnbc.com/id/100003114/device/rss/rss.html", #CNBC World财经/科技/市场焦点
+    "https://feeds.bbci.co.uk/news/technology/rss.xml",       #BBC 科技
+    "https://feeds.bbci.co.uk/news/business/rss.xml",        # BBC 商业
 ]
